414f80dca95684699cb79459862e454b83cb156c
RytisMk <76166574+RytisMk@users.noreply.github.com>
2020-12-17 15:35:02 +0200

    Pirma Uzduotis

---
+++ testukas/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace testukas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String:");
+            string testdata = Console.ReadLine();
+
+            if(testdata.Length > 0)
+                Console.WriteLine($"{testdata[0]}{testdata}{testdata[0]}");
+            else
+                Console.WriteLine("MORE LETTERS PLS");
+
+            Console.ReadLine();
+
+        }
+    }
+}
