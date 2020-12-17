dda998a4e18493bed393f0f297db53709dc52a3a
RytisMk <76166574+RytisMk@users.noreply.github.com>
2020-12-17 15:49:17 +0200

    Trecia Uzduotis

---
+++ testukas3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace testukas3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String:");
+            string testdata = Console.ReadLine();
+
+            char[] charArray = testdata.ToCharArray();
+            Array.Reverse( charArray );
+            Console.WriteLine(new string(charArray));
+            Console.ReadLine();
+        }
+    }
+}
