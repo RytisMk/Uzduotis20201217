62fbc166d30a9f5951828c68ec87adafe412c892
RytisMk <76166574+RytisMk@users.noreply.github.com>
2020-12-17 15:44:05 +0200

    Antra Uzduotis

---
+++ testukas2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace testukas2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter a month number:");
+            int monthNumber = Convert.ToInt32(Console.ReadLine());
+            DateTimeFormatInfo mfi = new DateTimeFormatInfo();
+            string strMonthName = mfi.GetMonthName(monthNumber).ToString();           
+            
+            Console.WriteLine(strMonthName);
+            Console.ReadLine();
+        }
+    }
+}
